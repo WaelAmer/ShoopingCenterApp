736b0498fd2e85dccb20932f206723f8d58aeae5
WaelAmer <wael.amer@gmail.com>
2015-10-24 07:05:50 +0200

    Init

--- src/BLL/ArticleCtr.java
+++ src/BLL/ArticleCtr.java
@@ -4,14 +4,7 @@ package BLL;
  * Created by A046098 on 23.10.2015.
  */
 public class ArticleCtr {
-    ArticleCtr _ArticleCtr;
-    InvoiceCtr _InvoiceCtr;
 
-
-    public ArticleCtr(){
-        _ArticleCtr=new ArticleCtr();
-        _InvoiceCtr=new InvoiceCtr();
-    }
 
     /**
      * Add new Article .
--- src/BLL/SysCtr.java
+++ src/BLL/SysCtr.java
@@ -4,4 +4,13 @@ package BLL;
  * Created by A046098 on 23.10.2015.
  */
 public class SysCtr {
+    ArticleCtr _ArticleCtr;
+    InvoiceCtr _InvoiceCtr;
+
+
+    public SysCtr(){
+        _ArticleCtr=new ArticleCtr();
+        _InvoiceCtr=new InvoiceCtr();
+    }
+
 }
