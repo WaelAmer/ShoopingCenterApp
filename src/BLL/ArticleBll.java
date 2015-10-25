e0b97bdfd64bc22fc1c7eb424ea37bf34fb0d829
WaelAmer <wael.amer@gmail.com>
2015-10-25 11:40:24 +0100

    DAL with SqlServer and Hibernate Entities

--- src/BLL/ArticleBll.java
+++ src/BLL/ArticleBll.java
@@ -3,6 +3,7 @@ package BLL;
 import DAL.ArticleEntity;
 import DAL.HibernateUtil;
 import DAL.IArticleMgr;
+import org.apache.log4j.PropertyConfigurator;
 import org.hibernate.Session;
 
 import java.util.List;
@@ -13,9 +14,15 @@ import java.util.logging.Logger;
  */
 public class ArticleBll {
     private IArticleMgr _Dal;
+    private static final org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(ArticleBll.class);
 
     public ArticleBll(IArticleMgr Dal){
         _Dal=Dal;
+        BindConfiguration();
+    }
+
+    private void BindConfiguration() {
+        PropertyConfigurator.configure("log4j.properties");
     }
     /**
      * Add new Article .
@@ -28,7 +35,7 @@ public class ArticleBll {
             Success=true;
         }
         catch (Exception Ex){
-            //TODO:Log Exception.
+            log.error(String.format("Business Layer Error at AddArticle : %s Detail Exception : %s", Article, Ex.getMessage()));
         }
         return Success;
     }
@@ -37,18 +44,44 @@ public class ArticleBll {
      * Edit Article .
      * @return true if success false if failed.
      */
-    public boolean EditArticle(ArticleEntity Article){
-        return false;
+    public boolean EditArticle(int ArticleID,ArticleEntity Article){
+        boolean Success=false;
+        try {
+            _Dal.Update(ArticleID,Article);
+            Success=true;
+        }
+        catch (Exception Ex){
+            log.error(String.format("Business Layer Error at EditArticle : %s Detail Exception : %s", ArticleID, Ex.getMessage()));
+        }
+        return Success;
     }
 
     /**
      * Delete Article .
      * @return true if success false if failed.
      */
-    public boolean DelArticle(int ArticleID){return false;}
+    public boolean DelArticle(int ArticleID){
+        boolean Success=false;
+        try {
+            _Dal.DeleteByID(ArticleID);
+            Success=true;
+        }
+        catch (Exception Ex){
+            log.error(String.format("Business Layer Error at EditArticle : %s Detail Exception : %s", ArticleID, Ex.getMessage()));
+        }
+        return Success;
+    }
 
     public List<ArticleEntity> GetAll(){
-        return _Dal.GetAll();
+        List<ArticleEntity> Result=null;
+        try {
+            return _Dal.GetAll();
+        }
+        catch (Exception Ex){
+            log.error(String.format("Business Layer Error at GetAll ,Detail Exception : %s",  Ex.getMessage()));
+        }
+        return Result;
+
     }
 
 
